b50220d5dbeb9b83c33f8d0caafcbbd04f49f5b3
Glen Sargent <glensarg@hotmail.co.uk>
2024-05-12 12:47:31 +0100

    chore: add scotty and handlers

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad.Reader
+import Database.SQLite.Simple as DB
+import Network.Wai.Middleware.Cors
+import Web.Scotty.Trans
+
+data HandlerEnv = HandlerEnv
+  { conn :: Connection
+  }
+
+type AppM = ReaderT HandlerEnv IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  appConn <- DB.open "app.db"
+  let env = HandlerEnv appConn
+  -- execute_ conn "CREATE TABLE IF NOT EXISTS scans (id INTEGER PRIMARY KEY, name TEXT)"
+  scottyT 3000 (`runReaderT` env) $ do
+    middleware simpleCors
+    get "/" $ handleIndex
+
+handleIndex :: ActionT AppM ()
+handleIndex = do
+  -- connection <- asks conn
+  file "index.html"
