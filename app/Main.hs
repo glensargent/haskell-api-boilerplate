d03f9a2eddb9278b98d3a99025cb4c43f6f1ab89
Glen Sargent <glensarg@hotmail.co.uk>
2024-05-12 13:09:14 +0100

    feat: url params and post json

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Control.Monad.Reader
 import Database.SQLite.Simple as DB
 import Network.Wai.Middleware.Cors
 import Web.Scotty.Trans
+import Data.Aeson
+import GHC.Generics
 
 data HandlerEnv = HandlerEnv
   { conn :: Connection
   }
 
 type AppM = ReaderT HandlerEnv IO
+
+data User = User {
+  name :: String,
+  age :: Int
+} deriving (Show, Generic)
+instance ToJSON User
+instance FromJSON User
+
 
 main :: IO ()
 main = do
@@ -20,8 +31,22 @@ main = do
   scottyT 3000 (`runReaderT` env) $ do
     middleware simpleCors
     get "/" $ handleIndex
+    get "/:name" $ handleParamExample
+    post "/user" $ handlePostExample
 
 handleIndex :: ActionT AppM ()
 handleIndex = do
   -- connection <- asks conn
   text "hello world"
+
+handleParamExample :: ActionT AppM ()
+handleParamExample = do
+  nameParam <- pathParam "name"
+  text ("hello " <> nameParam)
+
+handlePostExample :: ActionT AppM ()
+handlePostExample = do
+  reqBody <- jsonData
+  liftIO $ print $ name reqBody
+  liftIO $ print $ age reqBody
+
